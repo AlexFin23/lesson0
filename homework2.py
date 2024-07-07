f15f24d5ca5d528ceff08ea15789de0352386fc1
AlexFin23 <alexandra.finkelshtein@gmail.com>
2024-07-07 23:23:02 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,5 +1,5 @@
 homeworks="12,"
 hours="1,5,"
 cours="Python,"
-time_for_one_task="0.125 часа."
+time_for_one_task= 1,5/12
 print('Курс:',cours,'всего задач:',homeworks,'затрачено часов:',hours,'среднее время выполнения:',time_for_one_task)
