677dcf0c80c505422b6c322601c35fb6b2866d2b
AlexFin23 <alexandra.finkelshtein@gmail.com>
2024-07-07 21:46:45 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homeworks="12,"
+hours="1,5,"
+cours="Python,"
+time_for_one_task="0.125 часа."
+print('Курс:',cours,'всего задач:',homeworks,'затрачено часов:',hours,'среднее время выполнения:',time_for_one_task)
