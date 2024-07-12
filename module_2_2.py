cb8918d5cd4caf47b4dff46ceb48be3982be9e24
AlexFin23 <alexandra.finkelshtein@gmail.com>
2024-07-12 23:14:27 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,23 @@
+first = 123
+second = 456
+third = 789
+
+if first == second and second == third and third == first:
+    print(3)
+elif first==second or second==third or third==first:
+    print(2)
+else:
+    first!=second and second!=third and first!=third
+    print(0)
+
+first = 42
+second = 69
+third = 42
+
+if first == second and second == third and third == first:
+    print(3)
+elif first==second or second==third or third==first:
+    print(2)
+else:
+    first!=second and second!=third and first!=third
+    print(0)
