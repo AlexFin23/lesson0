3240c40fa413d62a807852926b57b6bdd9f9c431
AlexFin23 <alexandra.finkelshtein@gmail.com>
2024-07-16 22:43:30 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -4,6 +4,8 @@ not_primes = []
 for i in range(0, len(numbers)):
     is_prime = True
     if i > 0:
+        if i == 1:
+            continue
         for j in range(2, i):
             if (i % j) == 0:
                 is_prime = False
