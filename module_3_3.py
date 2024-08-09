1a7cb16e459d1de18ca33de1895017c6d858329d
AlexFin23 <alexandra.finkelshtein@gmail.com>
2024-08-09 22:55:27 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,9 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a,b,c)
+
+values_list = [23,24,26]
+values_dict = {'a': 33, 'b':'apple', 'c': False}
+values_list_2 = [54.32, 'Строка' ]
+print_params(*values_list_2, 42)
+# print_params(*values_list)
+# print_params(**values_dict)
