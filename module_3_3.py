19526627079b11973433bdcd97ecbe268d7c4821
AlexFin23 <alexandra.finkelshtein@gmail.com>
2024-08-09 23:01:05 +0300

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -4,6 +4,8 @@ def print_params(a = 1, b = 'строка', c = True):
 values_list = [23,24,26]
 values_dict = {'a': 33, 'b':'apple', 'c': False}
 values_list_2 = [54.32, 'Строка' ]
+# print_params(b = 25)
+# print_params(c = [1,2,3])
 print_params(*values_list_2, 42)
 # print_params(*values_list)
 # print_params(**values_dict)
